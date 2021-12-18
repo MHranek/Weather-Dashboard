20b4a5d40924e2fc3fde75b092447b2dc3555c8a
Michael Hranek <mhranek@comcast.net>
2021-12-18 10:03:22 -0500

    Favorited cities list added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,66 @@
 // Elements
 var searchBtnEl = $('#search-btn');
+var savedCitiesListEl = $('#saved-cities-list');
+var forecastSectionEl = $('#forecast-section');
+var inputCityEl = $('#input-city');
+
+var apiKey = '';
+var savedCities = [];
 
 // Search for a city
 var searchCity = function (event) {
     event.preventDefault();
     // get input from the user
-    var inputCity = $('#input-city').val().trim();
+    var inputCity = inputCityEl.val().trim();
     if (!inputCity) {
         console.log('You must type in a city name');
         return;
     } else {
+        // TODO log city name for testing
         console.log(inputCity);
     }
-    // fetch data from openWeatherAPI
-    
-    // call function to display data
+    // call retrieveCityData
+    // retrieveCityData(inputCity);
 
     // call function to save user search as a favorite city in the list
+    saveCityName(inputCity);
+}
 
+function saveCityName(city) {
+    // check if city is already saved
+    if (savedCities.includes(city)) {
+        return;
+    } else {
+        // create button element with city name as value
+        var favCityBtn = $('<button>');
+        favCityBtn.text(city);
+        favCityBtn.attr('class', 'my-1 col-12 bg-success');
+        savedCitiesListEl.append(favCityBtn);
+
+        // adding city to the array of saved cities, later to be kept on localstorage
+        savedCities.push(city);
+
+        // TODO if savedCities > 10 remove first element
+    }
+}
+
+function retrieveCityData(city) {
+    // create api url
+    var lat; // city's latitude
+    var lon; // city's longetude
+    var openWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=' + apiKey;
+    // fetch data from openWeatherAPI
+    fetch(openWeatherUrl).then(function (response){
+        if (response.ok) {
+            response.json().then(function (data) {
+                // do something with the data
+                // call function to display data
+                displayWeatherData(data);
+            })
+        } else {
+            console.log('Error retrieving data: ' + response.status);
+        }
+    });
 }
 
 searchBtnEl.on('click', searchCity);
